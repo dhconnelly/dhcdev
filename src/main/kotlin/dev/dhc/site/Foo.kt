3fcdc5f282de644bf743d65ea19e343dfd7dd094
Daniel Connelly <dhconnelly@gmail.com>
2025-06-27 08:22:37 +0200

    begin kotlinization

---
+++ src/main/kotlin/dev/dhc/site/Foo.kt
@@ -0,0 +1,3 @@
+package dev.dhc.site
+
+fun foo() = 5
---
+++ src/test/kotlin/dev/dhc/site/FooTest.kt
@@ -0,0 +1,11 @@
+package dev.dhc.site
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FooTest {
+    @Test
+    fun testFoo() {
+        assertEquals(5, foo())
+    }
+}
