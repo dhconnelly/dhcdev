40f8996a74d6becab77f23db838658e338da0327
Daniel Connelly <dhconnelly@gmail.com>
2025-07-02 10:32:27 +0200

    gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+    application
+}
+
+group = "dev.dhc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("com.squareup.moshi:moshi:1.15.2")
+    implementation("org.http4k:http4k-core:6.15.0.1")
+    implementation("org.http4k:http4k-server-jetty:6.15.0.1")
+    implementation("org.http4k:http4k-format-moshi:6.15.0.1")
+    implementation("org.http4k:http4k-config:6.15.0.1")
+    implementation("org.commonmark:commonmark:0.25.0")
+    implementation("org.commonmark:commonmark-ext-heading-anchor:0.25.0")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+application {
+    mainClass = "dev.dhc.site.MainKt"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
